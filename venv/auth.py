7b45d54ea97aa8aad79c76047fcf6abcb64e1dae
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-31 14:22:04 -0600

    added rendering to login and sign up routes

--- venv/auth.py
+++ venv/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/log_in')
 def log_in():
-    return "<p>Login</p>"
+    return render_template("log_in.html", boolean=True)
 
 
 @auth.route('log_out')
@@ -15,4 +15,4 @@ def log_out():
 
 @auth.route('/sign_up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
